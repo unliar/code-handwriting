18b873dd5828d0788d3cf08ff31c0c656c6cad3a
blessyou <370732889@qq.com>
2021-06-09 16:14:05 +0800

    feat: subset

---
+++ 6.大数相加.js
@@ -0,0 +1,20 @@
+const add = (a, b) => {
+  const maxLen = Math.max(a.length, b.length);
+  a = a.padStart(maxLen, 0);
+  b = b.padStart(maxLen, 0);
+  let res = "";
+  // 进位
+  let f = 0;
+  for (let index = maxLen - 1; index >= 0; index--) {
+    const temSum = f + parseInt(a[index]) + parseInt(b[index]);
+    // 获取进位1
+    f = ~~(temSum / 10);
+    res = (temSum % 10) + res;
+  }
+  if (f == 1) {
+    res = "1" + res;
+  }
+  return res;
+};
+
+console.log(add("92", "222"));
