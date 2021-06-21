2f870b9d4072e3fbe4fc96d786155646bfcf3a67
blessyou <370732889@qq.com>
2021-06-21 14:20:41 +0800

    feat: add stock price

---
+++ 23.股票买卖的最佳时间.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ * @description 获取一比股票交易最佳买入卖出收益
+ * @link https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock
+ */
+var maxProfit = function (prices) {
+  // 最小值
+  let min = prices[0];
+  // 收益
+  let profit = 0;
+  const len = prices.length;
+  for (let index = 0; index < len; index++) {
+    const price = prices[index];
+    // 最小股价
+    min = Math.min(price, min);
+
+    // 最大收益
+    profit = Math.max(price - min, profit);
+  }
+  return profit;
+};
