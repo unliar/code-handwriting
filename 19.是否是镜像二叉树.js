1dfbbaaadd2769b8c8076851dec696dffea48c9b
blessyou <370732889@qq.com>
2021-06-21 14:41:56 +0800

    feat: add stock price

--- 19.是否是镜像二叉树.js
+++ 19.是否是镜像二叉树.js
@@ -1,10 +1,26 @@
-const CheckMirror = (node) => {
-  const isMirror = (node1, node2) => {
-    if (!node1 && !node2) return true;
-    if (!node1 || !node2 || node1.val != node2.val) return false;
-    return (
-      isMirror(node1.left, node2.right) && isMirror(node1.right, node2.left)
-    );
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function (root) {
+  if (!root) return true;
+
+  const isMirror = (x, y) => {
+    if (x == null && y == null) {
+      return true;
+    }
+    if (x == null || y == null || x.val != y.val) {
+      return false;
+    }
+    return isMirror(x.left, y.right) && isMirror(x.right, y.left);
   };
-  return isMirror(node, node);
+
+  return isMirror(root, root);
 };
