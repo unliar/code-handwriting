54c9176638a845b40d1f1be1008064baecfe4d3d
blessyou <370732889@qq.com>
2021-06-23 16:01:22 +0800

    feat: add call bind apply

---
+++ 32.防抖节流.js
@@ -0,0 +1,24 @@
+// 💊点  每次都返回 一个函数
+
+function Denounce(fn, wait = 500) {
+  let timer = null;
+  return function () {
+    // 每次都clearTime
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      timer = null;
+      fn.apply(this, arguments);
+    }, wait);
+  };
+}
+
+function Throtte(fn, wait = 500) {
+  let timer = null;
+  return function () {
+    if (timer) return;
+    timer = setTimeout(() => {
+      timer = null;
+      fn.apply(this, arguments);
+    }, wait);
+  };
+}
