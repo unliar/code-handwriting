6aa3430775ebd04307bee754bf923f6c568d38bf
blessyou <370732889@qq.com>
2021-06-23 17:43:47 +0800

    feat: add url query parse

--- 34.实现url query parse.js
+++ 34.实现url query parse.js
@@ -1,9 +1,7 @@
-const Parse = (str) => {
+const ParseQuery = (str) => {
   const obj = {};
   str.replace(/([^?&=]+)=([^&]+)/g, (_, k, v) => {
     obj[k] = v;
   });
   return obj;
 };
-
-console.log(Parse("?a=1&c=2"));
