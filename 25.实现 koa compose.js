3171bc1b0dde3d2acaa6c41434236f810576e0b5
blessyou <370732889@qq.com>
2021-06-24 16:11:22 +0800

    feat: compose

--- 25.实现 koa compose.js
+++ 25.实现 koa compose.js
@@ -1,11 +1,29 @@
 // 核心代码
 // 返回一个koa中间件函数
 // 核心 dipatch 函数返回 promose resolve
+/**
+ *
+ * @param  middlewares
+ */
 const compose = (middlewares) => {
   return function (ctx, next) {
+    let index = -1;
     return dispatch(0);
     function dispatch(i) {
-      return Promise.resolve();
+      index = i;
+
+      let fn = middlewares[i];
+      if (i == middlewares.length) {
+        fn = next;
+      }
+      if (!fn) {
+        return Promise.resolve();
+      }
+      return Promise.resolve(
+        fn(ctx, function next() {
+          return dispatch(i + 1);
+        })
+      );
     }
   };
 };
