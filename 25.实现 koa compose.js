510ae6f913192c233661bee6a1864df21ed030a6
blessyou <370732889@qq.com>
2021-06-24 16:19:43 +0800

    feat: compose

--- 25.实现 koa compose.js
+++ 25.实现 koa compose.js
@@ -11,7 +11,6 @@ const compose = (middlewares) => {
     return dispatch(0);
     function dispatch(i) {
       index = i;
-
       let fn = middlewares[i];
       if (i == middlewares.length) {
         fn = next;
@@ -27,3 +26,28 @@ const compose = (middlewares) => {
     }
   };
 };
+
+async function test1(ctx, next) {
+  console.log("中间件1上");
+  await next();
+  console.log("中间件1下");
+}
+
+async function test2(ctx, next) {
+  console.log("中间件2上");
+  await next();
+  console.log("中间件2下");
+}
+
+async function test3(ctx, next) {
+  console.log("中间件3上");
+  await next();
+  console.log("中间件3下");
+}
+let middleware = [test1, test2, test3];
+
+let cp = compose(middleware);
+
+cp("ctx", function () {
+  console.log("中心");
+});
