3991eb3ff209ab49e50c9271652a2b207c878078
blessyou <370732889@qq.com>
2021-06-25 23:54:29 +0800

    feat: update random init

---
+++ 38.从n个数中同概率的选出m个数字.js
@@ -0,0 +1,23 @@
+/**
+ * 蓄水池抽样
+ * 同概率的从 n 个数字中选出n个数字
+ * @param {number} n
+ * @param {number} m
+ */
+const RandomPick = (n, m) => {
+  const res = [];
+  for (let i = 0; i < n; i++) {
+    // 数据初始化
+    if (i < m) {
+      res.push(i);
+    } else {
+      const random = ~~(Math.random() * (0, i));
+      if (random < m) {
+        res[random] = i;
+      }
+    }
+  }
+  return res;
+};
+
+console.log(RandomPick(5, 3));
