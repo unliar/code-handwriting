f975e9d072a743cf27ea5ee109896c25824b746a
blessyou <370732889@qq.com>
2021-06-28 11:29:05 +0800

    fix: 39 质数

--- 39.是否是质数.js
+++ 39.是否是质数.js
@@ -1,7 +1,19 @@
 const isPrime = (x) => {
-  for (let index = 2; index * index < x; ++index) {
-    if (x % index == 0) return false;
+  for (let index = 2; index * index <= x; ++index) {
+    if (x % index === 0) return false;
   }
   return true;
 };
-console.log(isPrime(67));
+console.log(isPrime(4));
+
+const PrimeList = (n) => {
+  const list = [];
+  for (let index = 2; index < n; index++) {
+    if (isPrime(index)) {
+      list.push(index);
+    }
+  }
+  return list;
+};
+
+console.log(PrimeList(100));
