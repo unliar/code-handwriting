e9775cb784193a1678a1ccd609cbffdb65fae362
blessyou <370732889@qq.com>
2021-06-30 11:12:15 +0800

    feat: 排序算法

--- 10.实现new.js
+++ 10.实现new.js
@@ -3,3 +3,10 @@ const New = (fn, ...args) => {
   const ret = fn.apply(obj, args);
   return ret instanceof Object ? ret : obj;
 };
+
+const Create = (proto) => {
+  function F() {}
+  F.prototype = proto; // 将原型挂在构造函数的prototype上
+  F.prototype.constructor = F;
+  return new F(); // 返回新对象
+};
