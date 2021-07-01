6af56399125256747d0133917fc42ad0a51d2e5f
blessyou <370732889@qq.com>
2021-07-01 22:23:03 +0800

    feat: update lazyman

---
+++ 44.LayzyMan.js
@@ -0,0 +1,51 @@
+class LM {
+  constructor(name) {
+    this.name = name;
+    this.q = [];
+    this.timer = null;
+    this.hi();
+  }
+  exec() {
+    clearTimeout(this.timer);
+    this.timer = setTimeout(async () => {
+      for (let index = 0; index < this.q.length; index++) {
+        await this.q[index]();
+      }
+    });
+    return this;
+  }
+  sleep(n) {
+    this.q.push(() => this._wait(n));
+    return this.exec();
+  }
+  sleepFirst(n) {
+    this.q.unshift(() => this._wait(n));
+    return this.exec();
+  }
+  eat(str) {
+    this.q.push(() => {
+      console.log("eat", str);
+    });
+    return this.exec();
+  }
+  hi() {
+    this.q.push(() => {
+      console.log("hi", this.name);
+    });
+    return this.exec();
+  }
+  _wait(time) {
+    return new Promise((y) => {
+      setTimeout(() => {
+        console.log("wake up after " + time + "s");
+        y();
+      }, time * 1000);
+    });
+  }
+}
+
+const LazyMan = (name) => {
+  return new LM(name);
+};
+
+LazyMan("雷峰").sleep(5).sleepFirst(2).eat("吃屎");
