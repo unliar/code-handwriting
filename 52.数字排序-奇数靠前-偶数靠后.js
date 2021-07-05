fc153196c346e4feae14e92d74d466475ceb5e32
blessyou <370732889@qq.com>
2021-07-05 13:50:22 +0800

    feat: 添加数组奇数靠前

---
+++ 52.数字排序-奇数靠前-偶数靠后.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ * @link https://leetcode-cn.com/problems/diao-zheng-shu-zu-shun-xu-shi-qi-shu-wei-yu-ou-shu-qian-mian-lcof/submissions/
+ */
+var exchange = function (nums) {
+  nums.sort((a, b) => (b % 2) - (a % 2));
+  return nums;
+};
