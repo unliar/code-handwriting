1b25766ffead9977883c1afe85e923ec13c18026
blessyou <370732889@qq.com>
2021-07-14 10:28:15 +0800

    feat: docs

---
+++ src/.vuepress/config.js
@@ -0,0 +1,55 @@
+const { description } = require("../../package");
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: "常见算法题解",
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ["meta", { name: "theme-color", content: "#3eaf7c" }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    [
+      "meta",
+      { name: "apple-mobile-web-app-status-bar-style", content: "black" },
+    ],
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: "",
+    editLinks: false,
+    docsDir: "",
+    editLinkText: "",
+    lastUpdated: true,
+    nav: [
+      {
+        text: "目录",
+        link: "/leetcode/",
+      },
+      {
+        text: "Github",
+        link: "https://github.com/unliar/code-handwriting",
+      },
+    ],
+    sidebar: "auto",
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: ["@vuepress/plugin-back-to-top", "@vuepress/plugin-medium-zoom"],
+};
---
+++ src/index.md
@@ -0,0 +1,17 @@
+---
+home: true
+lang: zh-CN
+heroText: 平平无奇的一个手写代码文档库
+heroImage: https://v1.vuepress.vuejs.org/hero.png
+tagline:
+actionText: 立刻开始 →
+actionLink: /leetcode/
+features:
+  - title: 思路清晰
+    details: 不追求最简洁,追求简单。
+  - title: 从零开始
+    details: 无需算法基础
+  - title: 偶尔更新
+    details: 当作者还能喘气
+footer: Made by  with 远浅 ❤️
+---
