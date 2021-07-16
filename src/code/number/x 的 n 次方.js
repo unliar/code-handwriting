ed4d8773ad5dc7f63062fc93fef10d826697a098
blessyou <370732889@qq.com>
2021-07-16 10:01:17 +0800

    feat: docs

--- src/code/number/x 的 n 次方.js
+++ src/code/number/x 的 n 次方.js
@@ -1,9 +1,13 @@
 /**
+ * 50. Pow(x, n)
+ *
+ * @description 实现 pow(x, n) ，即计算 x 的 n 次幂函数（即，xn）。
  * @param {number} x
  * @param {number} n
  * @return {number}
+ * @link https://leetcode-cn.com/problems/powx-n/
  */
-var myPow = function (x, n) {
+var myPow = function(x, n) {
   if (n == 0) return 1;
   // 负数
   if (n < 0) return 1 / myPow(x, -n);
