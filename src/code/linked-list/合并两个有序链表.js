edb97aa119b17911d48079b6c1bdfb63d8524607
blessyou <370732889@qq.com>
2021-07-16 13:51:03 +0800

    feat: docs

---
+++ src/code/linked-list/合并两个有序链表.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * 合并两个有序链表
+ *
+ * @description 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ * @link https://leetcode-cn.com/problems/merge-two-sorted-lists/
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+  if (!l1 || !l2) return l1 || l2;
+  // 递归 哪个比较小 就链接到哪条上
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l2.next, l1);
+    return l2;
+  }
+};
