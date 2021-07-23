348dc4232111f247a77b2a320b811480a861296b
blessyou <370732889@qq.com>
2021-07-23 10:42:59 +0800

    docs: update js array

--- src/code/number/剑指 Offer 10- I. 斐波那契数列.js
+++ src/code/number/剑指 Offer 10- I. 斐波那契数列.js
@@ -10,3 +10,11 @@ var fib = function(n) {
 
   return pre;
 };
+
+var fib2 = function(n) {
+  const dp = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  return dp[n];
+};
