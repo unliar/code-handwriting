7b8518a078720c5f8876d986d6e3b39e0db40eef
blessyou <370732889@qq.com>
2021-10-19 15:56:54 +0800

    feat: 单词翻转

--- src/code/string/剑指 Offer 58 - I. 翻转单词顺序.js
+++ src/code/string/剑指 Offer 58 - I. 翻转单词顺序.js
@@ -1,6 +1,7 @@
 /**
  * @param {string} s
  * @return {string}
+ * @description 原生API 两者测试用例无性能区别...
  */
 var reverseWords = function(s) {
   return s
@@ -9,4 +10,28 @@ var reverseWords = function(s) {
     .map((i) => i.trim())
     .reverse()
     .join(" ");
+};
+
+/**
+ * @param {string} s
+ * @return {string}
+ * @description 遍历字符按条件截断
+ */
+var reverseWords = function(s) {
+  let res = [];
+  let temp = "";
+  for (let str of s) {
+    // 如果字符串不为空 拼接字符串
+    if (str != " ") {
+      temp += str;
+    }
+    // 字符串为空且 temp 字符串不为空 说明该截断。
+    if (str == " " && temp != "") {
+      res = [temp].concat(res);
+      temp = "";
+    }
+  }
+  // 最后一个单词处理
+  if (temp != "") res = [temp].concat(res);
+  return res.join(" ");
 };
