9d46b2b9caf337500d988161572eb9093a8be832
blessyou <370732889@qq.com>
2021-10-19 16:17:25 +0800

    feat: 单词翻转

--- src/code/string/剑指 Offer 58 - I. 翻转单词顺序.js
+++ src/code/string/剑指 Offer 58 - I. 翻转单词顺序.js
@@ -24,14 +24,20 @@ var reverseWords = function(s) {
     // 如果字符串不为空 拼接字符串
     if (str != " ") {
       temp += str;
-    }
-    // 字符串为空且 temp 字符串不为空 说明该截断。
-    if (str == " " && temp != "") {
-      res = temp + " " + res;
-      temp = "";
+    } else {
+      // str 字符串为空且 temp 字符串不为空 说明该截断。
+      if (temp != "") {
+        res = c(temp, res);
+        temp = "";
+      }
     }
   }
   // 最后一个单词处理
-  if (temp != "") res = temp + " " + res;
-  return res.trim();
+  if (temp != "") res = c(temp, res);
+  return res;
+
+  // 为了不调用trim()方法
+  function c(temp, res) {
+    return res == "" ? temp : temp + " " + res;
+  }
 };
