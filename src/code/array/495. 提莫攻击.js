405ffd1185f9c02b91e7d956ea585d4b93d3aa6e
blessyou <370732889@qq.com>
2021-10-26 10:01:56 +0800

    feat: 提莫公鸡

---
+++ src/code/array/495. 提莫攻击.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} timeSeries
+ * @param {number} duration
+ * @return {number}
+ * @description 提莫攻击
+ * @link https://leetcode-cn.com/problems/teemo-attacking/
+ */
+var findPoisonedDuration = function(timeSeries, duration) {
+  let res = 0;
+  if (timeSeries.length == 0) return duration;
+  for (let index = 1; index < timeSeries.length; index++) {
+    const pre = timeSeries[index - 1];
+    const current = timeSeries[index];
+    const x = current - pre;
+    // 如果间隔小于duration，则按照间隔计算
+    if (x <= duration) {
+      res += x;
+    } else {
+      // 否则按照duration计算
+      res += duration;
+    }
+  }
+  return res + duration;
+};
