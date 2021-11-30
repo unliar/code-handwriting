9aa18e6b1c5c98c7cb7cfabbe10e1070a3f68c1d
blessyou <370732889@qq.com>
2021-11-30 21:26:39 +0800

    feat: update 二进制求和

--- src/code/number/67. 二进制求和.js
+++ src/code/number/67. 二进制求和.js
@@ -2,22 +2,23 @@
  * @param {string} a
  * @param {string} b
  * @return {string}
+ * @link https://leetcode-cn.com/problems/add-binary/
+ *
+ * @desc 给你两个二进制字符串，返回它们的和（用二进制表示）。
  */
 var addBinary = function (a, b) {
   const al = a.length;
   const bl = b.length;
   const max = Math.max(al, bl);
+  // 将两个字符串补齐
   a = a.padStart(max, "0");
-
   b = b.padStart(max, "0");
-
   let flag = 0;
   let res = "";
   for (let i = max - 1; i >= 0; i--) {
     const xa = a[i];
     const xb = b[i];
     const v = Number(xa) + Number(xb) + Number(flag);
-
     if (v <= 1) {
       res = `${v}${res}`;
       flag = 0;
