ada14a3b7e53b263b366823498ce9f36aa33b6ec
blessyou <370732889@qq.com>
2021-12-02 09:03:43 +0800

    feat: update 506名次

---
+++ src/code/array/506. 相对名次.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} score
+ * @return {string[]}
+ * @link https://leetcode-cn.com/problems/relative-ranks/
+ * @desc 给你一个长度为 n 的整数数组 score ，其中 score[i] 是第 i 位运动员在比赛中的得分。所有得分都 互不相同 。
+ * 运动员将根据得分 决定名次 ，其中名次第 1 的运动员得分最高，名次第 2 的运动员得分第 2 高，依此类推。运动员的名次决定了他们的获奖情况：
+ *
+ */
+var findRelativeRanks = function(score) {
+  const res = [];
+  const x = [...score].sort((a, b) => b - a);
+  for (let index = 0; index < score.length; index++) {
+    const v = score[index];
+    const t = x.findIndex((i) => i == v) + 1;
+    switch (t) {
+      case 1:
+        res.push("Gold Medal");
+        break;
+      case 2:
+        res.push("Silver Medal");
+        break;
+      case 3:
+        res.push("Bronze Medal");
+        break;
+      default:
+        res.push(`${t}`);
+    }
+  }
+  return res;
+};
