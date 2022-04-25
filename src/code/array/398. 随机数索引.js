6f70442c765067a9776cde018d17ed09b638eb0f
blessyou <370732889@qq.com>
2022-04-25 09:38:56 +0800

    feat: update 随机数索引

---
+++ src/code/array/398. 随机数索引.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @link {https://leetcode-cn.com/problems/random-pick-index/}
+ * @description 数组随机选择
+ */
+var Solution = function(nums) {
+  this.arr = nums;
+};
+
+/**
+ * @param {number} target
+ * @return {number}
+ */
+Solution.prototype.pick = function(target) {
+  let r;
+  let c = 0;
+  for (let i = 0; i < this.arr.length; i++) {
+    if (target == this.arr[i]) {
+      c++;
+      if (Math.random() < 1 / c) {
+        r = i;
+      }
+    }
+  }
+  return r;
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(nums)
+ * var param_1 = obj.pick(target)
+ */
